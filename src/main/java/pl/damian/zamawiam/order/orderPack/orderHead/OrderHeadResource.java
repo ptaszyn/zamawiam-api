f582225305115c66f61b154c915943024e0dbdb3
unknown <damian.ptaszynski@gmail.com>
2019-02-23 13:10:42 +0100

    RELEASE 0.0.1

--- src/main/java/pl/damian/zamawiam/order/orderPack/orderHead/OrderHeadResource.java
+++ src/main/java/pl/damian/zamawiam/order/orderPack/orderHead/OrderHeadResource.java
@@ -1,7 +1,9 @@
 package pl.damian.zamawiam.order.orderPack.orderHead;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/orderpacks/{orderPackId} /orderheads")
 public class OrderHeadResource {
 }
